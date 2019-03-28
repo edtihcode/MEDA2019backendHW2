e2d3aa6b2aae39f367750441d34fece4818a3580
Ed Code <EdCode@Nans-MacBook-Air.local>
2019-03-28 12:56:04 -0700

    added helicopter class

--- js/scripts.js
+++ js/scripts.js
@@ -40,20 +40,29 @@ class Bike extends Vehicle{
       this.numberOfGears=numberOfGears;
       this.wheelSize=wheelSize;
       this.material=material;
-
-    }
+      }
+  }
+class Helicopter extends Vehicle{
+    constructor(model, make, color, year, typeOfHeli, maxTakeOffWeight,maxRange, maxSpeed, power){
+      super(model, make, color, year, typeOfHeli, maxTakeOffWeight,maxRange, maxSpeed, power);// Must type this to inherits
+      this.typeOfHeli= typeOfHeli;        this.maxTakeOffWeight=maxTakeOffWeight;
+      this.maxRange=maxRange;
+      this.maxSpeed=maxSpeed;
+      this.power=power;
+      }
   }
 
-let car1 = new Vehicle("Accord","Black","Honda","2008","car");
-let car2 = new Vehicle("Camry","Red","Toyota","2017","car");
-let car3 = new Vehicle("x6","Blue","BMW","2019","car");
-let car4 = new Vehicle("SLR","Grey","Mercedes","2015","car");
-let car5 = new Vehicle("model X","White","Tesla","2018","car");
 
-let legitCar1 = new Car("Volkswagen", "Golf",2001,"Silver","1ASDAS",4,8,50000,"DIESEL");
-let legitCar2 = new Car("Elio", "One",2019,"Red","3S0DJAS",3,2,1000,"gasoline");
-let bike1 = new Bike("Specialized", "Model1",2020,"Silver","Mountain",12,"18\"","Aluminium");
+let car1 = new Vehicle("Accord","Honda","Black","2008","car");
+let car2 = new Vehicle("Camry","Toyota","Red","2017","car");
+let car3 = new Vehicle("x6","BMW","Blue","2019","car");
+let car4 = new Vehicle("SLR","Mercedes","Grey","2015","car");
+let car5 = new Vehicle("model X","Tesla","White","2018","car");
 
+let legitCar1 = new Car("Volkswagen", "Golf","Silver",2001,"1ASDAS",4,8,50000,"DIESEL");
+let legitCar2 = new Car("Elio", "One","Red",2019,"3S0DJAS",3,2,1000,"gasoline");
+let bike1 = new Bike("Specialized", "Model1","Silver",2020,"Mountain",12,"18\"","Aluminium");
+let heli1 = new Helicopter("Apache", "AH1", "grey", 2004, "military", "21000lb","331miles", "182mph", "2270hp");
 car1.showYear();
 car1.showMake();
 car5.showModel();
@@ -63,3 +72,4 @@ legitCar1.showYear();
 legitCar2.showMake();
 console.log(bike1.material);
 console.log(bike1.wheelSize);
+console.log(heli1.power);
