be3b2fbf63544869d93046094efe2484abbe87f6
Ed Code <EdCode@Nans-MacBook-Air.local>
2019-04-02 13:13:44 -0700

    added car, truck, motocycle and get and set

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@ console.log("Hello World!");
 class Vehicle {
 
   //constructor is a method for a class
-  constructor(model, make, color, year) {
+  constructor(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType) {
     this.model = model;
     this.color = color;
     this.make = make;
     this.year = year;
-
+    this.licensePlate = licensePlate;
+    this.wheelCount = wheelCount;
+    this.cylinders = cylinders;
+    this.miles = miles;
+    this.fuelType = fuelType;
 
   }
   showModel(){
@@ -22,17 +26,52 @@ class Vehicle {
   showYear() {
     console.log("The Year is "+this.year);
   }
+  getModel(){return this.model;}
+  setModel(data){this.model = data;}
+  getColor(){return this.color;}
+  setColor(data){this.color = data;}
+  getMake(){return this.make;}
+  setMakemake(data){this.make = data;}
+  getYear(){return this.year;}
+  setYear(data){this.year = data;}
+  getLicensePlate(){return this.licensePlate;}
+  setLicensePlate(data){this.licensePlate = data;}
+  getWheelCount(){return this.wheelCount;}
+  setWheelCount(data){this.wheelCount = data;}
+  getCylinders(){return this.cylinders;}
+  setCylinders(data){this.cylinders = data;}
+  getMiles(){return this.miles;}
+  setMiles(data){this.miles = data;}
+  getFuelType(){return this.FuelType;}
+  setFuelType(data){this.FuelType = data;}
 }
 class Car extends Vehicle{
-  constructor(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType){
-    super(model, make, color, year);// Must type this to inherits
-    this.licensePlate= licensePlate;
-    this.wheelCount=wheelCount;
-    this.cylinders=cylinders;
-    this.miles=miles;
-    this.fuelType=fuelType;
+  constructor(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType, trunkSize,reverseSpeed, tankSize){
+    super(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType, trunkSize,reverseSpeed, tankSize);// Must type this to inherits
+    this.trunkSize=trunkSize;
+    this.reverseSpeed=reverseSpeed;
+    this.tankSize=tankSize;
   }
+  getTrunkSize(){return this.trunkSize;}
+  setTrunkSize(data){this.trunkSize = data;}
+  getReverseSpeed(){return this.reverseSpeed;}
+  setReverseSpeed(data){this.reverseSpeed = data;}
+  getTankSize(){return this.tankSize;}
+  setTankSize(data){this.tankSize = data;}
 }
+class ElectricCar extends Vehicle{
+  constructor(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType, batterySize, currentCharge, batteryAge){
+    super(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType, batterySize, currentCharge, batteryAge);
+    this.batterySize=batterySize;
+    this.currentCharge=currentCharge;
+    this.batteryAge=batteryAge;
+  getBatterySize(){return this.batterySize;}
+  setBatterySize(data){this.batterySize = data;}
+  getcurrentCharge(){return this.currentCharge;}
+  setcurrentCharge(data){this.currentCharge = data;}
+  getBatteryAge(){return this.batteryAge;}
+  setBatteryAge(data){this.batteryAge = data;}
+  }
 class Bike extends Vehicle{
     constructor(model, make, color, year, bikeType,numberOfGears, wheelSize, material){
       super(model, make, color, year, bikeType,numberOfGears, wheelSize, material);// Must type this to inherits
@@ -42,6 +81,51 @@ class Bike extends Vehicle{
       this.material=material;
       }
   }
+class Truck extends Vehicle{
+  constructor(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType, tanksize, fuelType, topSpeed){
+    super(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType, tanksize, fuelType, topSpeed);// Must type this to inherits
+    this.trunkSize=trunkSize;
+    this.fuelType=fuelType;
+    this.topSpeed=topSpeed;
+  }
+  getTrunkSize(){return this.trunkSize;}
+  setTrunkSize(data){this.trunkSize = data;}
+  getFuelType(){return this.fuelType;}
+  setFuelType(data){this.fuelType = data;}
+  getTopSpeed(){return this.topSpeed;}
+  setTopSpeed(data){this.topSpeed = data;}
+}
+class Motocycle extends Vehicle{
+  constructor(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType, engineCC, tireSize, tankSize){
+    super(model, make, color, year, licensePlate,wheelCount,cylinders,miles,fuelType, engineCC, tireSize, tankSize);// Must type this to inherits
+    this.engineCC=engineCC;
+    this.tireSize=tireSize;
+    this.tankSize=tankSize;
+  }
+  getEngineCC(){return this.engineCC;}
+  setEngineCC(data){this.engineCC = data;}
+  getTireSize(){return this.tireSize;}
+  setTireSize(data){this.tireSize = data;}
+  getTankSize(){return this.tankSize;}
+  setTankSize(data){this.tankSize = data;}
+}
+class Bike extends Vehicle{
+    constructor(model, make, color, year, bikeType,numberOfGears, wheelSize, material){
+      super(model, make, color, year, bikeType,numberOfGears, wheelSize, material);// Must type this to inherits
+      this.bikeType= bikeType;
+      this.numberOfGears=numberOfGears;
+      this.wheelSize=wheelSize;
+      this.material=material;
+      }
+      getBikeType(){return this.bikeType;}
+      setBikeType(data){this.bikeType = data;}
+      getNumberOfGear(){return this.NumberOfGear;}
+      setNumberOfGear(data){this.NumberOfGear = data;}
+      getWheelSize(){return this.wheelSize;}
+      setWheelSize(data){this.wheelSize = data;}
+      getMaterial(){return this.material;}
+      setMaterial(data){this.material = data;}
+  }
 class Helicopter extends Vehicle{
     constructor(model, make, color, year, typeOfHeli, maxTakeOffWeight,maxRange, maxSpeed, power){
       super(model, make, color, year, typeOfHeli, maxTakeOffWeight,maxRange, maxSpeed, power);// Must type this to inherits
@@ -50,6 +134,7 @@ class Helicopter extends Vehicle{
       this.maxSpeed=maxSpeed;
       this.power=power;
       }
+      
   }
 
 
@@ -63,13 +148,14 @@ let legitCar1 = new Car("Volkswagen", "Golf","Silver",2001,"1ASDAS",4,8,50000,"D
 let legitCar2 = new Car("Elio", "One","Red",2019,"3S0DJAS",3,2,1000,"gasoline");
 let bike1 = new Bike("Specialized", "Model1","Silver",2020,"Mountain",12,"18\"","Aluminium");
 let heli1 = new Helicopter("Apache", "AH1", "grey", 2004, "military", "21000lb","331miles", "182mph", "2270hp");
-car1.showYear();
-car1.showMake();
-car5.showModel();
-car5.showMake();
-car5.showColor();
-legitCar1.showYear();
-legitCar2.showMake();
-console.log(bike1.material);
-console.log(bike1.wheelSize);
-console.log(heli1.power);
+
+// car1.showYear();
+// car1.showMake();
+// car5.showModel();
+// car5.showMake();
+// car5.showColor();
+// legitCar1.showYear();
+// legitCar2.showMake();
+// console.log(bike1.material);
+// console.log(bike1.wheelSize);
+// console.log(heli1.power);
